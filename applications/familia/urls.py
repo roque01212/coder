174057ba6e0e4a9b256921140b5cb2c0d2eb6d34
roque gerez <roque_gerez@hotmail.com>
2022-07-25 20:43:06 -0300

    creacion formulario

--- applications/familia/urls.py
+++ applications/familia/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name='familia_app'
+
 urlpatterns = [
-    path('formulario/', views.crear_familiares, name='Formulario'),
+    path('formulario/', views.FamiliaCreateView.as_view(), name='Formulario'),
+    path('lista/', views.FamiliaListView.as_view(), name='ListaFamiliares'),
 ]
--- applications/familia/views.py
+++ applications/familia/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
 from .forms import FamiliaForm
-
+from .models import Familia
 # Create your views here.
 
-def crear_familiares(request):
 
-    formulario=FamiliaForm
+class FamiliaCreateView(CreateView):
 
-    return render(request, 'familia/formulario.html', {'form':formulario})
+    template_name = "familia/add_familia.html"
+    model = Familia
+    form_class=FamiliaForm
+    success_url=reverse_lazy('familia_app:ListaFamiliares')
+
+ 
+class FamiliaListView(ListView):
+    template_name = "familia/listar.html"
+    model = Familia
+    context_object_name="lista"
+ 
