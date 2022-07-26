be9933d9cc4cd93f7d230c1ab31a9f7afb035ce0
roque gerez <roque_gerez@hotmail.com>
2022-07-26 12:37:23 -0300

    creacion de delete y update

--- applications/familia/urls.py
+++ applications/familia/urls.py
@@ -6,4 +6,6 @@ app_name='familia_app'
 urlpatterns = [
     path('formulario/', views.FamiliaCreateView.as_view(), name='Formulario'),
     path('lista/', views.FamiliaListView.as_view(), name='ListaFamiliares'),
+    path('delete/<pk>/', views.FamiliaDeleteView.as_view(), name='Delete'),
+    path('update/<pk>/', views.FamiliaUpdateView.as_view(), name='Update'),
 ]
--- applications/familia/views.py
+++ applications/familia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DeleteView, UpdateView
 from django.urls import reverse_lazy
 from .forms import FamiliaForm
 from .models import Familia
@@ -18,4 +18,19 @@ class FamiliaListView(ListView):
     template_name = "familia/listar.html"
     model = Familia
     context_object_name="lista"
+
+
+class FamiliaDeleteView(DeleteView):
+    model = Familia
+    template_name = "familia/delete.html"
+    success_url =  reverse_lazy('familia_app:ListaFamiliares')
+
+
+class FamiliaUpdateView(UpdateView):
+    model = Familia
+    template_name = "familia/update.html"
+    fields=('__all__')
+    success_url =  reverse_lazy('familia_app:ListaFamiliares')
+
+
  
